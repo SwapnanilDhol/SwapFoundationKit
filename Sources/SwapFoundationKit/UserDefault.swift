d31aa143cac14881680defa15057d70c2141cd9b
Swapnanil Dhol <swapnanildhol@gmail.com>
2025-05-31 18:57:03 +0530

    Add documentation to UserDefault property wrapper, box, protocol, and UserDefaults extension

--- Sources/SwapFoundationKit/UserDefault.swift
+++ Sources/SwapFoundationKit/UserDefault.swift
@@ -13,51 +13,79 @@ import Foundation
 
 import SwiftUI
 
+/// Protocol for UserDefault keys, allowing enums or other types to be used as keys.
 public protocol UserDefaultKeyProtocol {
+    /// The string value to use as the key in UserDefaults.
     var keyString: String { get }
 }
 
-@propertyWrapper
-public struct UserDefault<T: Equatable, Key: UserDefaultKeyProtocol>: DynamicProperty {
-    let key: Key
-    let defaultValue: T
-    var container: UserDefaults = .standard
-
-    @State private var value: T
-
-    public var wrappedValue: T {
-        get { value }
-        nonmutating set {
-            if value != newValue {
-                value = newValue
-                container.set(newValue, forKey: key.keyString)
+/// An observable box for a value stored in UserDefaults, used for property wrappers and SwiftUI bindings.
+///
+/// - Note: Only updates UserDefaults when the value actually changes.
+final class UserDefaultBox<T: Equatable>: ObservableObject {
+    /// The value stored in UserDefaults. Updates UserDefaults when changed.
+    @Published var value: T {
+        didSet {
+            if value != oldValue {
+                container.set(value, forKey: key)
             }
         }
     }
 
-    public var projectedValue: Binding<T> {
-        Binding(
-            get: { self.wrappedValue },
-            set: { self.wrappedValue = $0 }
-        )
+    private let key: String
+    private let container: UserDefaults
+
+    /// Initializes the box with a key, default value, and UserDefaults container.
+    /// - Parameters:
+    ///   - key: The key string for UserDefaults.
+    ///   - defaultValue: The default value to use if no value is stored.
+    ///   - container: The UserDefaults instance to use.
+    init(key: String, defaultValue: T, container: UserDefaults) {
+        self.key = key
+        self.container = container
+        let stored = container.object(forKey: key) as? T
+        self.value = stored ?? defaultValue
+    }
+}
+
+/// A property wrapper for type-safe, observable UserDefaults access with SwiftUI support.
+///
+/// Use with a key conforming to `UserDefaultKeyProtocol` and an `Equatable` value type.
+@propertyWrapper
+struct UserDefault<T: Equatable, Key: UserDefaultKeyProtocol>: DynamicProperty {
+    @ObservedObject private var box: UserDefaultBox<T>
+
+    /// The value stored in UserDefaults.
+    var wrappedValue: T {
+        get { box.value }
+        nonmutating set { box.value = newValue }
     }
 
-    public init(_ key: Key, default defaultValue: T, container: UserDefaults = .standard) {
-        self.key = key
-        self.defaultValue = defaultValue
-        self.container = container
-        let stored = container.object(forKey: key.keyString) as? T
-        _value = State(initialValue: stored ?? defaultValue)
+    /// A SwiftUI binding to the value.
+    var projectedValue: Binding<T> {
+        Binding(get: { self.box.value }, set: { self.box.value = $0 })
+    }
+
+    /// Initializes the property wrapper.
+    /// - Parameters:
+    ///   - key: The key to use for UserDefaults.
+    ///   - defaultValue: The default value if no value is stored.
+    ///   - container: The UserDefaults instance to use (default: .standard).
+    init(_ key: Key, default defaultValue: T, container: UserDefaults = .standard) {
+        self.box = UserDefaultBox(key: key.keyString, defaultValue: defaultValue, container: container)
     }
 }
 
 public extension UserDefaults {
-
+    /// Increments an integer counter for the given key in UserDefaults.
+    /// - Parameter key: The key for the counter.
     func incrementCounter(for key: UserDefaultKeyProtocol) {
         let currentValue = self.integer(forKey: key.keyString)
         self.set(currentValue + 1, forKey: key.keyString)
     }
 
+    /// Decrements an integer counter for the given key in UserDefaults.
+    /// - Parameter key: The key for the counter.
     func decrementCounter(for key: UserDefaultKeyProtocol) {
         let currentValue = self.integer(forKey: key.keyString)
         self.set(currentValue - 1, forKey: key.keyString)
