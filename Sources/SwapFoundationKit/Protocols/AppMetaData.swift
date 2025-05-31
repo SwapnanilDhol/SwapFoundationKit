233770c8d68c4e9770bf965453fe2010711c9efc
Swapnanil Dhol <swapnanildhol@gmail.com>
2025-05-31 16:43:42 +0530

    Add AppMetaData protocol file

---
+++ Sources/SwapFoundationKit/Protocols/AppMetaData.swift
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ * AppMetaData.swift
+ * SwapFoundationKit
+ *****************************************************************************
+ * Copyright (c) 2025 Swapnanil Dhol. All rights reserved.
+ *
+ * Authors: Swapnanil Dhol <swapnanildhol # gmail.com>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+ 
+ import Foundation
+
+ public protocol AppMetaData {
+    var appID: String { get }
+    var appName: String { get }
+    var appShareDescription: String { get }
+    var appInstagramUrl: URL? { get }
+    var appTwitterUrl: URL? { get }
+    var appWebsiteUrl: URL? { get }
+    var appPrivacyPolicyUrl: URL? { get }
+    var appEULAUrl: URL? { get }
+    var appSupportEmail: String? { get }
+ }
